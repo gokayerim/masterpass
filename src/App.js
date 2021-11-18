233ae6e4339fa5900d0176efdad908815e6dabdf
Gokay Erim <gerim@ebay.com>
2021-11-18 13:52:28 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: #332454;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+`;
+
+const Card = styled.div`
+  border: 1px solid #cad342;
+  border-radius: 6px;
+  background-color: #47bf8d;
+  width: 500px;
+  height: 100px;
+  margin: 10px;
+`;
+const TimerWrapper = styled.strong`
+  color: #eee;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
+  const [timer, setTimer] = useState(10);
+  useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => setTimer(timer - 1), 1000);
+    } else {
+      setTimeout(() => setTimer(10), 1000);
+    }
+  }, [timer]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>MasterPass'e hoş geldiniz</Title>
+      <TimerWrapper>Sayaç: {timer}</TimerWrapper>
+      <CardWrapper>
+        <Card />
+        <Card />
+        <Card />
+      </CardWrapper>
+    </Wrapper>
   );
 }
 
